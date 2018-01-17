823544de8c2d27ac694f83734d46ddee71547764
Simpson <Neil.Simpson@its.ny.gov>
2018-01-17 12:15:38 -0500

    Vue devtools working

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import router from './router';
 import App from './components/App.vue';
 
+Vue.config.devtools = true;
+
 window.onload = () => {
 	(<any>window).app = new Vue({
         router,
