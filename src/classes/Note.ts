f4efca9e6f06e6ffb504428da4bafb16a9113389
Simpson <Neil.Simpson@its.ny.gov>
2018-01-31 11:42:13 -0500

    Add Note.getNextNote to lookup next note by interval

--- src/classes/Note.ts
+++ src/classes/Note.ts
@@ -2,6 +2,8 @@ export default class Note {
 
 	public name: string;
 	public displayName: string;
+	private static notes: [Note];
+	private static loopedNotes: [any];
 
 	constructor(name: string, displayName: string) {
 		this.name = name;
@@ -12,21 +14,53 @@ export default class Note {
 		return Note.getAllNotes().find((note: Note) => (note as any)[propName] === value ) || null;
 	}
 
-	public static getAllNotes(): [Note] { // TODO: maybe make this a static property instead of static method?
-		return [
-			new Note('A', 'A'),
-			new Note('A#/Bb', 'A#'),
-			new Note('B', 'B'),
-			new Note('C', 'C'),
-			new Note('C#/Db', 'C#'),
-			new Note('D', 'D'),
-			new Note('D#/Eb', 'D#'),
-			new Note('E', 'E'),
-			new Note('F', 'F'),
-			new Note('F#/Gb', 'F#'),
-			new Note('G', 'G'),
-			new Note('G#/Ab', 'G#'),
-		];
+	public static getAllNotes(): [Note] {
+		if(!Note.notes){
+			Note.notes = [
+				new Note('A', 'A'),
+				new Note('A#/Bb', 'A#'),
+				new Note('B', 'B'),
+				new Note('C', 'C'),
+				new Note('C#/Db', 'C#'),
+				new Note('D', 'D'),
+				new Note('D#/Eb', 'D#'),
+				new Note('E', 'E'),
+				new Note('F', 'F'),
+				new Note('F#/Gb', 'F#'),
+				new Note('G', 'G'),
+				new Note('G#/Ab', 'G#'),
+			];
+		}
+
+		return Note.notes;
+	}
+
+	public static getLoopedNotes(): [any] {
+		if(!Note.loopedNotes){
+			Note.loopedNotes = (Note.notes.map((n: Note, i: number) => {
+				return {
+					prev: i - 1 < 0 ? Note.notes[Note.notes.length - 1] : Note.notes[i - 1],
+					next: i + 1 >= Note.notes.length ? Note.notes[0] : Note.notes[i + 1],
+					note: n
+				};
+			}) as [any]);
+		}
+
+		return Note.loopedNotes;
+	}
+
+	public getNextNote(interval: number): Note {
+		const loopedNotes: [any] = Note.getLoopedNotes();
+		const dir: string = interval > 0 ? 'next' : 'prev';
+
+		let nextNote: Note = this;
+		interval = Math.abs(interval);
+		while(interval--){
+			const startIndex: number = loopedNotes.findIndex(loopedNote => loopedNote.note.name === nextNote.name);
+			nextNote = loopedNotes[startIndex][dir];
+		}
+
+		return nextNote;
 	}
 
 }
