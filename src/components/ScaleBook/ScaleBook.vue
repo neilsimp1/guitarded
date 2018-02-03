2157f913e700ffbd9f0557bb7935df501d06efb6
neilsimp1 <neilsimp1@gmail.com>
2018-02-03 18:33:40 -0500

    Add ScaleControls seperate component

--- src/components/ScaleBook/ScaleBook.vue
+++ src/components/ScaleBook/ScaleBook.vue
@@ -2,32 +2,8 @@
 	<div>
 
 		<div class="scale-controls">
-
+			<ScaleControls />
 			<GuitarControls />
-
-			<div class="panel panel-scale-controls">
-				<label for="key">Key</label>
-				<select id="key" v-on:change="updateKey">
-					<option v-for="note in allNotes"
-						 :value="note.name"
-						 :key="note.name"
-						 :selected="key === note.name">
-						{{ note.displayName }}
-					</option>
-				</select>
-
-				<label for="scale">Scale</label>
-				<select id="scale" v-on:change="updateScale">
-					<option v-if="scales"
-						 v-for="_scale in scales"
-						 :key="_scale.name"
-						 :value="_scale.name"
-						 :selected="_scale.name === scale.name">
-						{{ _scale.name }}
-					</option>
-				</select>
-			</div>
-
 		</div>
 
 		<Guitar :noteSetSource="'scale'" />
@@ -40,47 +16,15 @@ import Vue from 'vue';
 import Component from 'vue-class-component';
 import Guitar from '../Common/Guitar.vue';
 import GuitarControls from '../Common/GuitarControls.vue';
+import ScaleControls from './ScaleControls.vue';
 import Note from '../../classes/Note';
 import Scale from '../../classes/Scale';
 
 @Component({
 	name: 'scalebook',
-	components: { Guitar, GuitarControls }
+	components: { Guitar, GuitarControls, ScaleControls }
 })
 export default class ScaleBook extends Vue {
-
-	public get scales(): any {
-		return this.$store.getters.scales;
-	}
-	public get scale(): Scale {
-		return this.$store.getters.scale;
-	}
-	public get key(): string {
-		return this.$store.getters.key;
-	}
-	public get allNotes(): [Note] {
-		return Note.getAllNotes();
-	}
-
-	public beforeCreate(): void {
-		if(!this.$store.getters.scales){
-			const scales: any = Scale.getScales();
-			this.$store.commit('updateScales', scales);
-			const scale = new Scale(scales[0].name, scales[0].intervals, this.$store.getters.key);
-			this.$store.commit('updateScale', scale);
-		}
-	}
-
-	public updateKey(event: Event): void {
-		this.$store.commit('updateKey', (event.target as HTMLSelectElement).value);
-		const newScale = new Scale(this.scale.name, this.scale.intervals, (event.target as HTMLSelectElement).value);
-		this.$store.commit('updateScale', newScale);
-	}
-
-	public updateScale(event: Event): void {
-		const newScale: Scale = Scale.getScale((event.target as HTMLSelectElement).value, this.key);
-		this.$store.commit('updateScale', newScale);
-	}
 
 }
 </script>
---
+++ src/components/ScaleBook/ScaleControls.vue
@@ -0,0 +1,73 @@
+<template>
+	<div class="panel panel-scale-controls">
+
+		<label for="key">Key</label>
+		<select id="key" v-on:change="updateKey">
+			<option v-for="note in allNotes"
+					:value="note.name"
+					:key="note.name"
+					:selected="key === note.name">
+				{{ note.displayName }}
+			</option>
+		</select>
+
+		<label for="scale">Scale</label>
+		<select id="scale" v-on:change="updateScale">
+			<option v-if="scales"
+					v-for="_scale in scales"
+					:key="_scale.name"
+					:value="_scale.name"
+					:selected="_scale.name === scale.name">
+				{{ _scale.name }}
+			</option>
+		</select>
+
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import Note from '../../classes/Note';
+import Scale from '../../classes/Scale';
+
+@Component({
+	name: 'scalecontrols'
+})
+export default class ScaleControls extends Vue {
+
+	public get scales(): any {
+		return this.$store.getters.scales;
+	}
+	public get scale(): Scale {
+		return this.$store.getters.scale;
+	}
+	public get key(): string {
+		return this.$store.getters.key;
+	}
+	public get allNotes(): [Note] {
+		return Note.getAllNotes();
+	}
+
+	public beforeCreate(): void {
+		if(!this.$store.getters.scales){
+			const scales: any = Scale.getScales();
+			this.$store.commit('updateScales', scales);
+			const scale = new Scale(scales[0].name, scales[0].intervals, this.$store.getters.key);
+			this.$store.commit('updateScale', scale);
+		}
+	}
+
+	public updateKey(event: Event): void {
+		this.$store.commit('updateKey', (event.target as HTMLSelectElement).value);
+		const newScale = new Scale(this.scale.name, this.scale.intervals, (event.target as HTMLSelectElement).value);
+		this.$store.commit('updateScale', newScale);
+	}
+
+	public updateScale(event: Event): void {
+		const newScale: Scale = Scale.getScale((event.target as HTMLSelectElement).value, this.key);
+		this.$store.commit('updateScale', newScale);
+	}
+
+}
+</script>
