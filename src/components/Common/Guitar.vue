a727fbeb73c413033cf482daf59a8b0db4d09452
neilsimp1 <neilsimp1@gmail.com>
2018-02-04 23:00:45 -0500

    Stylin

--- src/components/Common/Guitar.vue
+++ src/components/Common/Guitar.vue
@@ -3,7 +3,7 @@
 		<table v-if="fretboard">
 			<tr v-for="numFret in numFrets + 1" :key="numFret">
 				<td v-for="(gstring, index) in fretboard" :key="index">
-					{{ gstring.frets[numFret - 1] ? 1 : 0 }}
+					{{ gstring.frets[numFret - 1] ? 'O' : '|' }}
 				</td>
 			</tr>
 		</table>
