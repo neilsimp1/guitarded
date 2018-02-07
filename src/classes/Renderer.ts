2e4acbf6077a65ce69dd01d7f81f9cf99c21dd9f
Neil Simpson <Neil.Simpson@its.ny.gov>
2018-02-07 10:19:46 -0500

    Add breakpoints as constants

--- src/classes/Renderer.ts
+++ src/classes/Renderer.ts
@@ -4,6 +4,13 @@ export default class Renderer {
 	protected ctx: CanvasRenderingContext2D;
 	protected scale: number;
 
+	protected BP_XS: number = 0;
+	protected BP_SM: number = 576;
+	protected BP_MD: number = 768;
+	protected BP_LG: number = 992;
+	protected BP_XL: number = 1200;
+	protected BP_XXL: number = 1500;
+
 	constructor(canvas: HTMLCanvasElement) {
 		canvas.width = canvas.parentElement!.clientWidth;
 		canvas.height = 1000;
