9f8791dd74918cea3cf116e82d6d6929994a944c
neilsimp1 <neilsimp1@gmail.com>
2018-02-26 06:43:14 -0500

    wip

--- src/classes/AudioPlayer.ts
+++ src/classes/AudioPlayer.ts
@@ -3,29 +3,44 @@ import Pitch from './Pitch';
 export default class AudioPlayer {
 
 	private static ctx: AudioContext;
+	private ctx: AudioContext;
 
 	constructor() {
 		if(!AudioPlayer.ctx){
 			AudioPlayer.ctx = new AudioContext();
 		}
+		this.ctx = AudioPlayer.ctx;
 	}
 
-	public playPitch(pitch: Pitch, time: number, duration: number = 0.75): void {
-		const oscNode: OscillatorNode = AudioPlayer.ctx.createOscillator();
-		const gainNode: GainNode = AudioPlayer.ctx.createGain();
-		oscNode.connect(gainNode);
-		gainNode.connect(AudioPlayer.ctx.destination);
-		gainNode.gain.exponentialRampToValueAtTime(
-			0.00001, AudioPlayer.ctx.currentTime + duration + time
-		);
-		oscNode.frequency.value = pitch.frequency;
-		oscNode.start(time);
+	public playPitch(pitch: Pitch, duration: number = 0.75): void {
+		
 	}
 
-	public playPitches(pitches: Pitch[]): void {
-		for(let i = 0; i < pitches.length; i++) {
-			this.playPitch(pitches[i], i * 2);
-		}
+	public playPitches(pitches: Pitch[], duration: number = 0.75): void {
+		const start = (pitch: Pitch) => {
+			//oscNode.connect(gainNode);
+			//gainNode.connect(this.ctx.destination);
+			oscNode.type = 'sine';
+			oscNode.connect(this.ctx.destination);
+			oscNode.frequency.value = pitch.frequency;
+			oscNode.start();
+		};
+		const stop = () => {
+			oscNode.stop();
+			oscNode.disconnect(this.ctx.destination);
+		};
+
+		const oscNode: OscillatorNode = this.ctx.createOscillator();
+		//const gainNode: GainNode = this.ctx.createGain();
+
+		let i: number = 0;
+
+		start(pitches[i]);
+		setTimeout(() => {
+			stop();
+			i++;
+			if(i < pitches.length) start(pitches[i]);
+		}, duration);
 	}
 
 }
