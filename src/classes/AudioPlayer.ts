bbb5e51075b6a43e6df75282fca254beea4ec3f6
Simpson <Neil.Simpson@its.ny.gov>
2018-03-07 15:19:30 -0500

    AudioPlayer using one ctx

--- src/classes/AudioPlayer.ts
+++ src/classes/AudioPlayer.ts
@@ -6,93 +6,105 @@ interface IAmp {
 	gainNode: GainNode;
 }
 
+// interface Window {
+// 	AudioContext: typeof AudioContext;
+// 	webkitAudioContext: typeof AudioContext;
+// }
+// declare const window: Window;
+
 export default class AudioPlayer {
 
-	private createAmp(): IAmp {
-		const ctx: AudioContext = new AudioContext();
-		const oscNode: OscillatorNode = ctx.createOscillator();
-		const distNode: WaveShaperNode = ctx.createWaveShaper();
-		const gainNode: GainNode = ctx.createGain();
+	private static ctx: AudioContext;
+	private ctx: AudioContext;
+	private oscTypes: OscillatorType[] = ['sine', 'square', 'sawtooth', 'triangle'];
 
-		gainNode.connect(distNode);
-		gainNode.gain.value = 0.5;
-		distNode.connect(ctx.destination);
-		distNode.curve = this.makeDistortionCurve(1000);
-		oscNode.connect(gainNode);
-		oscNode.type = 'sine';
-		oscNode.connect(ctx.destination);
-
-		return { ctx, oscNode, gainNode };
+	constructor() {
+		//const AudioContextDecl = window.AudioContext || window.webkitAudioContext;
+		if(!AudioPlayer.ctx) AudioPlayer.ctx = new AudioContext();
+		this.ctx = AudioPlayer.ctx;
 	}
 
-	private makeDistortionCurve(amount: number): Float32Array {
-		const numSamples: number = 44100,
-			deg: number = Math.PI / 180;
-		let curve = new Float32Array(numSamples),
-			x: number;
-		for (let i = 0; i < numSamples; ++i ) {
-			x = i * 2 / numSamples - 1;
-			curve[i] = (3 + amount) * x * 20 * deg / (Math.PI + amount * Math.abs(x));
-		}
+	// private createAmp(): IAmp {
+	// 	const ctx: AudioContext = new AudioContext();
+	// 	const oscNode: OscillatorNode = ctx.createOscillator();
+	// 	const distNode: WaveShaperNode = ctx.createWaveShaper();
+	// 	const gainNode: GainNode = ctx.createGain();
 
-		return curve;
-	}
+	// 	gainNode.connect(distNode);
+	// 	gainNode.gain.value = 0.5;
+	// 	distNode.connect(ctx.destination);
+	// 	distNode.curve = this.makeDistortionCurve(1000);
+	// 	oscNode.connect(gainNode);
+	// 	oscNode.type = 'sine';
+	// 	oscNode.connect(ctx.destination);
+
+	// 	return { ctx, oscNode, gainNode };
+	// }
+
+	// private makeDistortionCurve(amount: number): Float32Array {
+	// 	const numSamples: number = 44100,
+	// 		deg: number = Math.PI / 180;
+	// 	let curve = new Float32Array(numSamples),
+	// 		x: number;
+	// 	for (let i = 0; i < numSamples; ++i ) {
+	// 		x = i * 2 / numSamples - 1;
+	// 		curve[i] = (3 + amount) * x * 20 * deg / (Math.PI + amount * Math.abs(x));
+	// 	}
+
+	// 	return curve;
+	// }
 
 	public playSequence(pitches: Pitch[], duration: number = 750): Promise<void> {
-		const loop = (i: number): Promise<void> => {
+		const start = (i: number): Promise<void> => {
 			return new Promise<void>((resolve: Function) => {
-				start(pitches[i]);
-				(function _loop(i) {
+				oscNode.frequency.value = pitches[i].frequency;
+				oscNode.start();
+				(function loop(i) {
 					if(i === pitches.length) return resolve();
 					setTimeout(() => {
-						stop();
 						i++;
-						if(i < pitches.length) start(pitches[i]);
-						_loop(i);
+						if(i < pitches.length) oscNode.frequency.value = pitches[i].frequency;
+						loop(i);
 					}, duration);
 				})(0);
 			});
 		};
-		const start = (pitch: Pitch) => {
-			amp.oscNode = amp.ctx.createOscillator();
-			amp.oscNode.connect(amp.gainNode);
-			//amp.oscNode.type = 'sine';
-			amp.oscNode.connect(amp.ctx.destination);
-			amp.oscNode.frequency.value = pitch.frequency;
-			amp.oscNode.start();
-		};
-		const stop = () => {
-			amp.oscNode.stop();
-			amp.oscNode.disconnect(amp.ctx.destination);
-		};
 
-		const amp: IAmp = this.createAmp();
+		const oscNode: OscillatorNode = this.ctx.createOscillator();
+		oscNode.type = this.oscTypes[1];
+		const gainNode: GainNode = this.ctx.createGain();
+		oscNode.connect(gainNode);
+		gainNode.connect(this.ctx.destination);
 
 		return new Promise<void>((resolve: Function) => {
-			loop(0).then(() => {
-				amp.ctx.close();
+			start(0).then(() => {
+				oscNode.stop();
 				resolve();
 			});
 		});
 	}
 
 	public playTogether(pitches: Pitch[], duration: number = 750): Promise<void> {
-		let amps: IAmp[] = [];
-		for(const pitch of pitches){
-			const amp: IAmp = this.createAmp();
-			//amp.gainNode.gain.value = 0.25;
-			amp.oscNode.frequency.value = pitch.frequency;
-			amps.push(amp);
-			amp.oscNode.start();
+		const getType = (i: number) => this.oscTypes[i % 4];
+
+		let oscs: OscillatorNode[] = [];
+		let gain: number = 1;
+		for(let i = 0; i < pitches.length; i++){
+			const oscNode: OscillatorNode = this.ctx.createOscillator();
+			const gainNode: GainNode = this.ctx.createGain();
+			oscNode.frequency.value = pitches[i].frequency;
+			oscNode.type = getType(i);
+			gainNode.gain.value = gain;
+			oscNode.connect(gainNode);
+			gainNode.connect(this.ctx.destination);
+			oscs.push(oscNode);
+			oscNode.start();
+			if(gain >= 0.4) gain -= 0.1;
 		}
 
 		return new Promise<void>((resolve: Function) => {
 			setTimeout(() => {
-				for(const amp of amps){
-					amp.oscNode.stop();
-					amp.oscNode.disconnect(amp.ctx.destination);
-					amp.ctx.close();
-				}
+				for(const oscNode of oscs) oscNode.stop();
 				resolve();
 			}, duration);
 		});
