73e22f622071de381417c35f147a7b200bd64bea
neilsimp1 <neilsimp1@gmail.com>
2018-03-11 14:46:19 -0400

    Guitar pick method

--- src/classes/AudioPlayer.ts
+++ src/classes/AudioPlayer.ts
@@ -33,7 +33,7 @@ export default class AudioPlayer {
 		oscNode.type = oscType;
 
 		const distNode: WaveShaperNode = this.ctx.createWaveShaper();
-		distNode.curve = this.distCurve
+		distNode.curve = this.distCurve;
 
 		const gainNode: GainNode = this.ctx.createGain();
 		gainNode.gain.value = gain;
@@ -59,29 +59,25 @@ export default class AudioPlayer {
 	}
 
 	public playSequence(pitches: Pitch[], duration: number = 750): Promise<void> {
-		const start = (i: number): Promise<void> => {
+		const play = (): Promise<void> => {
 			return new Promise<void>((resolve: Function) => {
-				effects.oscNode.frequency.value = pitches[i].frequency;
+				effects.gainNode.gain.value = 0;
 				effects.oscNode.start();
-				(function loop(i) {
+
+				(async function asyncLoop(i, _this) {
 					if(i === pitches.length) return resolve();
-					setTimeout(() => {
-						i++;
-						if(i < pitches.length){
-							effects.gainNode.gain.value = 0;
-							effects.oscNode.frequency.value = pitches[i].frequency;
-							setTimeout(_ => effects.gainNode.gain.value = 1, 60);
-						}
-						loop(i);
-					}, duration);
-				})(0);
+					
+					effects.oscNode.frequency.value = pitches[i].frequency;
+					await _this.pick(effects, duration);
+					asyncLoop(i + 1, _this);
+				})(0, this);
 			});
 		};
 
 		const effects: IEffectChain = this.createEffectChain();
 
 		return new Promise<void>((resolve: Function) => {
-			start(0).then(() => {
+			play().then(() => {
 				effects.oscNode.stop();
 				resolve();
 			});
@@ -106,6 +102,49 @@ export default class AudioPlayer {
 		return new Promise<void>((resolve: Function) => {
 			setTimeout(() => {
 				for(const oscNode of effects.oscNodes!) oscNode.stop();
+				resolve();
+			}, duration);
+		});
+	}
+
+	private async pick(effects: IEffectChain, duration: number): Promise<void> {
+		const MAX_VOLUME: number = 0.6;
+		const MAX_VOLUME_OVERTONE: number = 0.05;
+		const seconds: number = duration / 1000;
+
+		const overtoneGain = this.ctx.createGain();
+		overtoneGain.gain.value = 0;
+
+		const overtone1 = this.ctx.createOscillator();
+		overtone1.type = 'triangle';
+		overtone1.frequency.value = effects.oscNode.frequency.value;
+		overtone1.detune.value = 330;
+
+		const overtone2 = this.ctx.createOscillator();
+		overtone2.type = 'triangle';
+		overtone2.frequency.value = effects.oscNode.frequency.value;
+		overtone2.detune.value = 550;
+
+		overtone1.connect(overtoneGain);
+		overtone2.connect(overtoneGain);
+		overtoneGain.connect(this.ctx.destination);
+		//overtone1.start();
+		overtone2.start();
+
+		effects.oscNode.type = 'triangle';
+		
+		effects.gainNode.gain.setValueAtTime(0, this.ctx.currentTime);
+
+		effects.gainNode.gain.setValueAtTime(MAX_VOLUME, this.ctx.currentTime);		
+		overtoneGain.gain.setValueAtTime(MAX_VOLUME_OVERTONE, this.ctx.currentTime);
+
+		effects.gainNode.gain.exponentialRampToValueAtTime(0.05, this.ctx.currentTime + (seconds * 0.75));
+		overtoneGain.gain.exponentialRampToValueAtTime(0.01, this.ctx.currentTime + (seconds * 0.75));
+		
+		return new Promise<void>((resolve) => {
+			setTimeout(_ => { 
+				//overtone1.stop();
+				overtone2.stop();
 				resolve();
 			}, duration);
 		});
